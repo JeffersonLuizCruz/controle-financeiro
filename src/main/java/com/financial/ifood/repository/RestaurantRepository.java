5784fbd99b3a25ada05cca4bef39088199c05ba9
Jeff <jlcruz@stefanini.com>
2022-12-04 23:43:39 -0300

    adiciona consulta inteligente

--- src/main/java/com/financial/ifood/repository/RestaurantRepository.java
+++ src/main/java/com/financial/ifood/repository/RestaurantRepository.java
@@ -1,18 +1,23 @@
 package com.financial.ifood.repository;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.financial.ifood.domain.model.Restaurant;
 
 @Repository
 public interface RestaurantRepository extends JpaRepository<Restaurant, Long>{
-	
-	//@Query("FROM Restaurant r JOIN FETCH r.kitchen LEFT JOIN FETCH r.paymentMethods")
-	@Query("FROM Restaurant r JOIN FETCH r.kitchen")
+	//Tipo de consulta para reduzir os select a base de dados.
+	@Query("FROM Restaurant r JOIN FETCH r.kitchen LEFT JOIN FETCH r.paymentMethods LEFT JOIN FETCH r.owner LEFT JOIN FETCH r.products")
+	//@Query("FROM Restaurant r JOIN FETCH r.kitchen")
 	List<Restaurant> findAll();
+	
+	@Query("FROM Restaurant r JOIN FETCH r.kitchen LEFT JOIN FETCH r.paymentMethods LEFT JOIN FETCH r.owner LEFT JOIN FETCH r.products WHERE r.id = :id")
+	Optional<Restaurant> findById(@Param("id") Long id);
 
 }
