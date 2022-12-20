b2b8fdd31297665505d46df6adc0a45bf169e3d3
JeffersonLuizCruz <jefferson.luiz.cruz@gmail.com>
2022-12-20 13:39:56 -0300

    aidicona dto

--- src/main/java/com/financial/ifood/controller/StateController.java
+++ src/main/java/com/financial/ifood/controller/StateController.java
@@ -38,7 +38,8 @@ public class StateController {
 	}
 	
 	@PutMapping("/{id}")
-	public ResponseEntity<State> update(@PathVariable Long id, @RequestBody State state){
+	public ResponseEntity<State> update(@PathVariable Long id, @RequestBody @Valid StateDTO dto){
+		State state = stateMapper.toModel(dto);
 		return ResponseEntity.ok(stateService.update(id, state));
 	}
 	
