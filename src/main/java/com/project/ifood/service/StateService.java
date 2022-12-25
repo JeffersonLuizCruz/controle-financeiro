011142876fa40bcc12b8b1c670b8e255baf4a299
JeffersonLuizCruz <jefferson.luiz.cruz@gmail.com>
2022-12-25 19:24:36 -0300

    aidiciona controlador de estado

---
+++ src/main/java/com/project/ifood/service/StateService.java
@@ -0,0 +1,14 @@
+package com.project.ifood.service;
+
+import java.util.List;
+
+import com.project.ifood.domain.model.State;
+
+public interface StateService {
+	State save(State state);
+	State update(Long id, State state);
+	List<State> findAll();
+	State findById(Long id);
+	void deleteById(Long id);
+	State checkIfStateExists(Long id);
+}
