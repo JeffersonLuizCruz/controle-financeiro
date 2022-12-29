bc98b8f77b6747217d44c72116579cd9f3fea3b7
JeffersonLuizCruz <jefferson.luiz.cruz@gmail.com>
2022-12-29 18:48:50 -0300

    Adiciona controller

---
+++ src/main/java/com/project/ifood/domain/controller/KitchenController.java
@@ -0,0 +1,54 @@
+package com.project.ifood.domain.controller;
+
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.project.ifood.domain.model.Kitchen;
+import com.project.ifood.domain.service.KitchenService;
+
+@RestController
+@RequestMapping("kitchens")
+public class KitchenController {
+	
+	private final KitchenService kitchenService;
+	
+	public KitchenController(KitchenService kitchenService) {
+		this.kitchenService = kitchenService;
+	}
+
+	@PostMapping
+	public ResponseEntity<Kitchen> save(@RequestBody Kitchen kitchen) {
+		return ResponseEntity.status(HttpStatus.CREATED).body(kitchenService.save(kitchen));
+	}
+	
+	@PutMapping("/{id}")
+	public ResponseEntity<Kitchen> update(@PathVariable Long id, @RequestBody Kitchen kitchen){
+		return ResponseEntity.ok(kitchenService.update(id, kitchen));
+	}
+	
+	@GetMapping
+	public ResponseEntity<List<Kitchen>> findAll(){
+		return ResponseEntity.ok(kitchenService.findAll());
+	}
+
+	@GetMapping("/{id}")
+	public ResponseEntity<Kitchen> findById(@PathVariable Long id){
+		return ResponseEntity.ok(kitchenService.findById(id));
+	}
+	
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Void> delete(@PathVariable Long id){
+		kitchenService.deleteById(id);
+		return ResponseEntity.noContent().build();
+	}
+}
---
+++ src/main/java/com/project/ifood/domain/controller/RestaurantController.java
@@ -0,0 +1,57 @@
+package com.project.ifood.domain.controller;
+
+import java.util.List;
+
+import javax.validation.Valid;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.project.ifood.domain.model.Restaurant;
+import com.project.ifood.domain.service.RestaurantService;
+
+@RestController
+@RequestMapping("/restaurants")
+public class RestaurantController {
+	
+	private final RestaurantService restaurantService;
+
+	public RestaurantController(RestaurantService restaurantService) {
+		this.restaurantService = restaurantService;
+	}
+	
+	@PostMapping
+	public ResponseEntity<Restaurant> save(@RequestBody @Valid Restaurant restaurant){
+		return ResponseEntity.status(HttpStatus.CREATED).body(restaurantService.save(restaurant));
+	}
+	
+	@PutMapping("/{id}")
+	public ResponseEntity<Restaurant> update(@PathVariable Long id, @RequestBody Restaurant restaurant){
+		return ResponseEntity.ok(restaurantService.update(id, restaurant));
+	}
+	
+	@GetMapping
+	public ResponseEntity<List<Restaurant>> findAll(){
+		return ResponseEntity.ok(restaurantService.findAll());
+	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<Restaurant> findById(@PathVariable Long id){
+		return ResponseEntity.ok(restaurantService.findById(id));
+	}
+	
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Void> delete(@PathVariable Long id){
+		restaurantService.deleteById(id);
+		return ResponseEntity.notFound().build();
+	}
+ 
+}
