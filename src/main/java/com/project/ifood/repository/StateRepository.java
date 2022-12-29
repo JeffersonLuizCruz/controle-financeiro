432553b61adf0a95d5cd6858dfc9970f556e4b97
JeffersonLuizCruz <jefferson.luiz.cruz@gmail.com>
2022-12-29 10:28:07 -0300

    remove all

--- src/main/java/com/project/ifood/repository/StateRepository.java
+++
@@ -1,9 +0,0 @@
-package com.project.ifood.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.project.ifood.domain.model.State;
-
-public interface StateRepository extends JpaRepository<State, Long>{
-
-}
