a1dcebd4f4e530c1ca757c446c5729a63b7aa29c
JeffersonLuizCruz <jefferson.luiz.cruz@gmail.com>
2022-12-29 17:47:57 -0300

    Adiciona entidades

---
+++ src/main/java/com/project/ifood/domain/model/Product.java
@@ -0,0 +1,45 @@
+package com.project.ifood.domain.model;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor @NoArgsConstructor @Data
+@Entity
+public class Product implements Serializable{
+	private static final long serialVersionUID = 1524345699491374384L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(nullable = false)
+	private String name;
+	@Column(nullable = false)
+	private String description;
+	@Column(nullable = false)
+	private BigDecimal price;
+	@Column(nullable = false)
+	private Boolean isActive;
+	@Valid
+	@NotNull(message = "Entidade restaurant é obrigatório!")
+	@JsonBackReference // https://www.baeldung.com/jackson-bidirectional-relationships-and-infinite-recursion
+	@ManyToOne
+	@JoinColumn(nullable = false)
+	private Restaurant restaurant;
+}
---
+++ src/main/java/com/project/ifood/domain/model/Role.java
@@ -0,0 +1,29 @@
+package com.project.ifood.domain.model;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor @AllArgsConstructor @Data
+@Entity
+public class Role implements Serializable{
+	private static final long serialVersionUID = -4620654949492261615L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(nullable = false)
+	private String name;
+	
+	@Column(nullable = false)
+	private String description;
+}
