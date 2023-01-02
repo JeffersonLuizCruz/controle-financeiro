a319d30858127c732b808c3fb2bb89dd74c394c5
JeffersonLuizCruz <jefferson.luiz.cruz@gmail.com>
2023-01-01 22:17:15 -0300

    adiciona notenull

--- src/main/java/com/project/ifood/domain/controller/mapper/dto/CityResume.java
+++ src/main/java/com/project/ifood/domain/controller/mapper/dto/CityResume.java
@@ -10,7 +10,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor @AllArgsConstructor @Data
 public class CityResume {
 
-	@NotNull
+	 @NotNull(message = "A inserção da Cidade(object='city') é obrigatório!")
 	private Long id;
 
 	private String name;
