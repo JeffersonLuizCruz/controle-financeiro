3ab3310e2f93ebe788ec27cfd569136c3ba0f63d
JeffersonLuizCruz <jefferson.luiz.cruz@gmail.com>
2023-01-12 15:57:42 -0300

    hotfix: Resolve problema N+1

--- src/main/java/com/project/ifood/domain/repositoy/CityRepository.java
+++ src/main/java/com/project/ifood/domain/repositoy/CityRepository.java
@@ -1,6 +1,9 @@
 package com.project.ifood.domain.repositoy;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.project.ifood.domain.model.City;
@@ -8,4 +11,6 @@ import com.project.ifood.domain.model.City;
 @Repository
 public interface CityRepository extends JpaRepository<City, Long>{
 
+	@Query("FROM City c JOIN FETCH c.state")
+	List<City> findAll();
 }
--- src/main/java/com/project/ifood/domain/repositoy/GroupRepository.java
+++ src/main/java/com/project/ifood/domain/repositoy/GroupRepository.java
@@ -1,6 +1,11 @@
 package com.project.ifood.domain.repositoy;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.project.ifood.domain.model.Group;
@@ -8,4 +13,9 @@ import com.project.ifood.domain.model.Group;
 @Repository
 public interface GroupRepository extends JpaRepository<Group, Long>{
 	
+	@Query("FROM Group g JOIN FETCH g.roles")
+	List<Group> findAll();
+	
+	@Query("FROM Group g JOIN FETCH g.roles WHERE g.id = :id")
+	Optional<Group> findById(@Param("id") Long id);
 }
