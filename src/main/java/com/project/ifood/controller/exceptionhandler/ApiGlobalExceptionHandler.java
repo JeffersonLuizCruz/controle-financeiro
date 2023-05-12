b8b37ae48bd364da75ebc0fdf1e0bddf11b1889f
JeffersonLuizCruz <jefferson.luiz.cruz@gmail.com>
2023-05-12 14:07:50 -0300

    feat: adiciona exception handler

--- src/main/java/com/project/ifood/controller/exceptionhandler/ApiGlobalExceptionHandler.java
+++ src/main/java/com/project/ifood/controller/exceptionhandler/ApiGlobalExceptionHandler.java
@@ -11,6 +11,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.validation.BindException;
+import org.springframework.web.HttpMediaTypeNotAcceptableException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -65,6 +66,11 @@ public class ApiGlobalExceptionHandler extends ResponseEntityExceptionHandler {
 		return ResponseEntity.status(HttpStatus.BAD_REQUEST.value()).body(apiError);
 	}
 
+	@Override
+	protected ResponseEntity<Object> handleHttpMediaTypeNotAcceptable(HttpMediaTypeNotAcceptableException ex,
+			HttpHeaders headers, HttpStatus status, WebRequest request) {
+		return ResponseEntity.status(status).headers(headers).build();
+	}
 	@ExceptionHandler(ConstraintViolationService.class)
 	public ResponseEntity<ApiError> handlerConstraintViolation(ConstraintViolationService ex) {
 
