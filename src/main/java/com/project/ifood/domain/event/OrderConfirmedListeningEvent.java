82941ea01a085f3497ed0f0ec222eb2f4d60659d
JeffersonLuizCruz <jefferson.luiz.cruz@gmail.com>
2023-05-16 20:55:16 -0300

    feat: adiciona cors globalmente

--- src/main/java/com/project/ifood/domain/event/OrderConfirmedListeningEvent.java
+++ src/main/java/com/project/ifood/domain/event/OrderConfirmedListeningEvent.java
@@ -1,7 +1,7 @@
 package com.project.ifood.domain.event;
 
-import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.event.TransactionalEventListener;
 
 import com.project.ifood.infrastructure.service.SendMailService;
 import com.project.ifood.infrastructure.service.SendMailService.Message;
@@ -13,7 +13,8 @@ public class OrderConfirmedListeningEvent {
 
 	private final SendMailService sendMailService; // Injeção é um @Bean: package com.project.ifood.infrastructure.util.email
 	
-	@EventListener
+	//@EventListener
+	@TransactionalEventListener // O envio de email só vai acontecer depois da transação do banco estiver commitada.
 	public void sendEmailWhenConfirmingOrder(OrderConfirmedEvent orderConfirmedEvent) {
 		Message message = Message.builder()
 		.subject("Pedido " + orderConfirmedEvent.getOrder().getCode() + "confirmado com sucesso!")
